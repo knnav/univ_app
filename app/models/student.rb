b8e04d6308c071955cb7f6f88aa8264bf10c5d33
Kurt <kurt.norenbergs@gmail.com>
2020-01-27 11:22:13 -0300

    added password to students model and forms, no full auth yet

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,6 @@ class Student < ApplicationRecord
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { minimum: 2,  maximum: 50 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+  has_secure_password
 end
